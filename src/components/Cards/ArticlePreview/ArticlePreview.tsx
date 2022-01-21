3b1622aebae645e8513a82279954955f98182b74
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-01-21 15:42:10 +0100

    Added article preview card component

---
+++ src/components/Cards/ArticlePreview/ArticlePreview.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent } from "react";
+import styles from "../Cards.module.css";
+import Image from "next/image";
+const ArticlePreview: FunctionComponent = (props) => {
+  return (
+    <div className={styles.articlePreviewContainer}>
+      <div className={styles.articlePreviewChildOneContainer}>
+        <div className={styles.articlePreviewChildOneContainerInnerContainer}>
+          <div className={styles.articlePreviewNameImageContainer}>
+            <Image
+              src="/img/test-1.jpg"
+              layout="responsive"
+              width={10}
+              height={10}
+              alt="Picture of the author"
+              sizes="50vw"
+            />
+          </div>
+          <h6 className={styles.articlePreviewName}>
+            Temiloluwa Ojo et al <span className="font-normal">in</span>{" "}
+            Software Architecture
+          </h6>
+        </div>
+        <h2 className={styles.articlePreviewTitle}>
+          Why Do We Sleep? Science May Have Finally Figured It Out
+        </h2>
+        <div className={styles.articlePreviewMeta}>
+          <p>Date here</p>
+          <p>· 5 min read</p>
+          <p>Tag Here</p>
+        </div>
+      </div>
+      <div className={styles.articlePreviewChildTwoContainer}>
+        <Image
+          src="/img/test-2.jpeg"
+          layout="fill"
+          objectFit="cover"
+          alt="Picture of the author"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ArticlePreview;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,21 @@ import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import Category from "@/components/Category";
+import ArticlePreview from "@/components/Cards/ArticlePreview";
 const Home: NextPage = (props) => {
   return (
     <section className={styles.container}>
       <aside className={styles.asideSection}>
         <Category />
       </aside>
-      <section className={styles.contentSection}>Section </section>
+      <section className={styles.contentSection}>
+        <ArticlePreview />
+        <ArticlePreview />
+        <ArticlePreview />
+        <ArticlePreview />
+      </section>
     </section>
   );
 };
-
+  
 export default Home;
