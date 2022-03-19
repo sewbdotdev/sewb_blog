e661f8143b928b9115dcb52dc01664c6d805643d
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-03-19 12:13:25 +0100

    :hammer: added support for editing on TextBox component

--- src/components/Comment/TextBox/TextBox.tsx
+++ src/components/Comment/TextBox/TextBox.tsx
@@ -1,13 +1,22 @@
 import React, { FunctionComponent, useState } from "react";
 
-const TextBox: FunctionComponent = (props) => {
-  const [text, setText] = useState("");
+type TextBoxProps = {
+  value?: string;
+  autoFocus?: boolean;
+};
+
+const TextBox: FunctionComponent<TextBoxProps> = (props) => {
+  const { value = "", autoFocus = false } = props;
+  const [text, setText] = useState(value);
   return (
     <div className="flex flex-col mb-5 md:w-full">
       <textarea
         className={`rounded-md max-w-full h-${
           text.length > 0 ? "36" : "10"
-        } p-2 dark:bg-gray-700 shadow-xl focus:outline-none` }
+        } p-2 dark:bg-gray-700 shadow-xl focus:outline-none`}
+        autoFocus={autoFocus}
+        defaultValue={value}
+        onFocus={(e) => e.target.setSelectionRange(text.length, text.length)}
         placeholder="Speak, the world is listening..."
         value={text}
         onChange={(e) => setText(e.target.value)}
