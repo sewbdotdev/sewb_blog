536c03db9fd82e92eda9b55c6f34bb8c3ff6a756
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-03-29 11:48:03 +0200

    :hammer: added google analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
     const [queryClient] = React.useState(() => new QueryClient());
@@ -29,6 +30,23 @@ function MyApp({ Component, pageProps }: AppProps) {
                                 }
                             ]}
                         />
+                        {process.env.NODE_ENV === 'production' && (
+                            <>
+                                <Script
+                                    strategy="lazyOnload"
+                                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                                />
+
+                                <Script strategy="lazyOnload">
+                                    {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+                                `}
+                                </Script>
+                            </>
+                        )}
                         <Component {...pageProps} />
                     </Layout>
                 </ThemeProvider>
