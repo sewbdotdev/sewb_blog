643d3416fc5af97c3597a1d8d6811cb200656703
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-03-29 12:11:19 +0200

    :hammer: added google analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                                 window.dataLayer = window.dataLayer || [];
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
-                                gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                                 `}
                                 </Script>
                             </>
