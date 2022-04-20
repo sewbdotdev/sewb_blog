fa51e276fc230cd95a3669ff8e8829904faa0012
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-04-20 10:09:44 +0200

    :sparkles: changed blog favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,45 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body className="dark:bg-black bg-gray-200 dark:text-gray-200">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="/img/favicon/favicon.ico" />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/img/favicon/favicon-512x512.png"
+                    />
+
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/img/favicon/favicon-512x512.png"
+                    />
+
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="/img/favicon/favicon-192x192.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="512x512"
+                        href="/img/favicon/favicon-512x512.png"
+                    />
+                </Head>
+                <body className="dark:bg-black bg-gray-200 dark:text-gray-200">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
